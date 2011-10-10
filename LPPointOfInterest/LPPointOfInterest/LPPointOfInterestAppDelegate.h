ce629a3951d26f0f65802db545d380ac41fa75c4
Joshua Worby <joshua.worby.dev@gmail.com>
2011-10-10 01:38:07 -0700

    Added initial project setup

---
+++ LPPointOfInterest/LPPointOfInterest/LPPointOfInterestAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  LPPointOfInterestAppDelegate.h
+//  LPPointOfInterest
+//
+//  Created by Joshua Worby on 11-10-10.
+//  Copyright 2011 Team Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPPointOfInterestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ LPPointOfInterest/LPPointOfInterest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LPPointOfInterest
+//
+//  Created by Joshua Worby on 11-10-10.
+//  Copyright 2011 Team Pixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
